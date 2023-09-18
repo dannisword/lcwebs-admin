db4c81bd569fe683e7d58fbd4ef6a0555807e9bc
dannis <dannis.word@gmail.com>
2023-09-18 10:59:43 +0800

    fix page-handle

--- src/hooks/page-handle.ts
+++ src/hooks/page-handle.ts
@@ -1,23 +1,11 @@
 import { ref, computed, onBeforeUnmount, onMounted } from "vue";
 import { useI18n } from "vue-i18n";
 import { routeHandle } from "../hooks/route-handle";
-import * as store from '../store';
 
 export function pageHandle() {
      const container = reactive({ isLoading: false, message: "載入中" });
      const { t, locale } = useI18n();
-     const { navigation } = routeHandle();
-
-     onMounted(() => {
-          console.log("onMounted");
-     })
-
-     onBeforeUnmount(() => {
-          console.log('onBeforeUnmount');
-     })
      return {
-          container,
-          navigation,
-          store
+          container
      }
 }
--- src/hooks/route-handle.ts
+++ src/hooks/route-handle.ts
@@ -3,15 +3,6 @@ import { useRouter, useRoute } from "vue-router";
 export function routeHandle() {
      const router = useRouter();
      const route = useRoute();
-     const appRoutes = Array<any>();
-     /**
-      * 重定位
-      * @param url 
-      */
-     const redirect = (url: any) => {
-          const redirectUrl = route.query && route.query.redirect;
-          console.log(route.query.redirect);
-     }
      /**
       * 導覽頁面
       * @param url 
@@ -19,36 +10,8 @@ export function routeHandle() {
      const navigation = (url: any) => {
           router.push({ path: url });
      }
-     /**
-      * 
-      * @returns 
-      */
-     const currentRouteValue = () => {
-          //  route 全部 
-          // console.log(router.getRoutes());
-          return router.currentRoute;
-     }
-     const addViews = () => {
-          appRoutes.push(router.currentRoute.value.meta);
-
-          // localStorage.setItem('app-view', JSON.stringify(appRoutes))
-     }
-
-     const getAllViews = () => {
-          const data = localStorage.getItem('app-view');
-
-          if (data == null) {
-               return data;
-          }
-          console.log(JSON.parse(data));
-          return JSON.parse(data);
-     }
      return {
-          redirect,
-          navigation,
-          currentRouteValue,
-          getAllViews,
-          addViews
+          navigation
      }
 }
 
